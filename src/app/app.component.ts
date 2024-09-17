74d253d303e8325fa8c1a319d219739dfa5f46d5
softprotechcoader <softprotechcoader@outlook.com>
2024-09-17 18:49:43 +0530

    Control flow with for loop.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,6 +26,13 @@ export class UserComponent{
     @else{
       <div>No, the server is not running</div>
     }
+    @for (OS of operetingSystems; track OS.id){
+      {{OS.name}}
+    }
+    @for (user of users; track user.id){
+      <p>{{user.name}}</p>
+    }
+
   `,
   styles: `
     :host {
@@ -39,4 +46,16 @@ export class AppComponent {
   city = 'San Francisco';
 
   isServerRunning:boolean = true;
+
+  operetingSystems :{id:string, name:string}[] = [
+    {id: 'win', name: 'Windows'}, 
+    {id: 'osx', name: 'MacOS'}, 
+    {id: 'linux', name: 'Linux'}];
+
+    users:{id:number,name:string}[]=[
+      {id: 0, name: 'Sarah'}, 
+      {id: 1, name: 'Amy'}, 
+      {id: 2, name: 'Rachel'}, 
+      {id: 3, name: 'Jessica'}, 
+      {id: 4, name: 'Poornima'}];
 }
