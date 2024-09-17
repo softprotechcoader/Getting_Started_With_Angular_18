10e28bb1105073dcf0ead7112d40c8e163234355
softprotechcoader <softprotechcoader@outlook.com>
2024-09-17 17:06:10 +0530

    Composing component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
 import {Component} from '@angular/core';
 
 @Component({
+  selector: 'app-user',
+  template: `
+  UserName :{{ username }}`,
+  standalone: true,
+
+})
+export class UserComponent{
+  username = ' YoungTech ';
+}
+
+
+
+@Component({
   selector: 'app-root',
   template: `
     Hello {{city}} , {{1+1}}
+    <section>
+    <app-user />
+    </section>
   `,
   styles: `
     :host {
@@ -11,6 +27,7 @@ import {Component} from '@angular/core';
     }
   `,
   standalone: true,
+  imports: [UserComponent]
 })
 export class AppComponent {
   city = 'San Francisco'
