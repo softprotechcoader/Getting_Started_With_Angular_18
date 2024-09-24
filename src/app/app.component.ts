ab1111d5d8166c0f2470e0496aac3070cafd8e2f
softprotechcoader <softprotechcoader@outlook.com>
2024-09-24 12:28:07 +0530

    Event Handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,7 +7,7 @@ import {Component} from '@angular/core';
   standalone: true,
 
 })
-export class UserComponent{
+export class UserComponent {
   username = ' YoungTech ';
 }
 
@@ -35,6 +35,10 @@ export class UserComponent{
     <!-- contentEditable enables to edit the content of div -->
 
     <div [contentEditable] = "isEditable">This is a Div</div>
+    <button class="btn" (click)="greet()" >click here</button>
+    <section (mouseover)="onMouseOver()">There's a secret message for you, hover to reveal:
+      {{ message }}
+    </section>
     
 
   `,
@@ -42,28 +46,45 @@ export class UserComponent{
     :host {
       color: #a144eb;
     }
+    .btn{
+      background-color: black;
+    color: navajowhite;
+    font-size: larger;
+    padding: 4px;
+    size: auto;
+    margin: 22px 100px;
+    }
   `,
   standalone: true,
   imports: [UserComponent]
 })
 export class AppComponent {
+    message: string = '';
+  onMouseOver(){
+    this.message = "Way to Go 🚀";
+  }
+
+  greet() {
+    alert("Welcome to Angular 18 👋");
+  }
+
   city = 'San Francisco';
 
-  isServerRunning:boolean = true;
+  isServerRunning: boolean = true;
 
-  isEditable : boolean = true;
+  isEditable: boolean = true;
 
-  operetingSystems :{id:string, name:string}[] = [
-    {id: 'win', name: 'Windows'}, 
-    {id: 'osx', name: 'MacOS'}, 
-    {id: 'linux', name: 'Linux'}];
+  operetingSystems: { id: string, name: string }[] = [
+    { id: 'win', name: 'Windows' },
+    { id: 'osx', name: 'MacOS' },
+    { id: 'linux', name: 'Linux' }];
 
-    users:{id:number,name:string}[]=[
-      {id: 0, name: 'Sarah'}, 
-      {id: 1, name: 'Amy'}, 
-      {id: 2, name: 'Rachel'}, 
-      {id: 3, name: 'Jessica'}, 
-      {id: 4, name: 'Poornima'}];
+  users: { id: number, name: string }[] = [
+    { id: 0, name: 'Sarah' },
+    { id: 1, name: 'Amy' },
+    { id: 2, name: 'Rachel' },
+    { id: 3, name: 'Jessica' },
+    { id: 4, name: 'Poornima' }];
 
 }
 
